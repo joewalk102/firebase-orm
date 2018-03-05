9c826d6d9b91fc4ed16edf7a2de2919e1196a62d
zavx0z <zavx0z@ya.ru>
2018-03-06 00:25:36 +0700

    add Manager._get_ref_col, refactor Manager._id_autoincrement

--- firebase_orm/models/manager.py
+++ firebase_orm/models/manager.py
@@ -36,19 +36,21 @@ class Manager:
         return obj
 
     def _id_autoincrement(self):
-        doc_ref = self.db.collection('test')
-        doc = doc_ref.order_by('id', direction=firestore.Query.DESCENDING).limit(1).get()
-        for d in doc:
+        docs = self._get_ref_col().order_by('id', direction=firestore.Query.DESCENDING).limit(1).get()
+        for d in docs:
             pk = int(d.id) + 1
             return pk
 
-    def _get_ref(self, pk):
+    def _get_ref_col(self):
         db_table = self._model.Meta.db_table
-        doc_ref = self.db.collection(db_table).document(str(pk))
+        return self.db.collection(db_table)
+
+    def _get_ref_doc(self, pk):
+        doc_ref = self._get_ref_col().document(str(pk))
         return doc_ref
 
     def _get_data(self, pk):
-        doc_ref = self._get_ref(pk)
+        doc_ref = self._get_ref_doc(pk)
         try:
             doc = doc_ref.get()
             data = doc.to_dict()
@@ -57,5 +59,5 @@ class Manager:
         return data
 
     def _save(self, pk, meta):
-        doc_ref = self._get_ref(pk)
+        doc_ref = self._get_ref_doc(pk)
         doc_ref.update(meta, firestore.CreateIfMissingOption(True))
