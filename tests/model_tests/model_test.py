8b737b166519610f6c76ed104832020b8107c71d
zavx0z <zavx0z@ya.ru>
2018-03-06 10:04:08 +0700

    add TestEqual

--- tests/model_tests/model_test.py
+++ tests/model_tests/model_test.py
@@ -1,15 +1,21 @@
-def test_equal_obj(model):
-    """содержимое экземпляров равно"""
-    inst = model.objects.get(id=1)
-    inst1 = model.objects.get(id=1)
-    assert inst == inst1
+class TestEqual:
 
+    def test_equal_obj(self, model):
+        """содержимое экземпляров равно"""
+        inst = model.objects.get(id=1)
+        inst1 = model.objects.get(id=1)
+        assert inst == inst1
 
-def test_not_equal_obj(model):
-    """содержимое экземпляров не равно"""
-    inst = model.objects.get(id=1)
-    inst1 = model()
-    assert inst != inst1
+    def test_not_equal_obj(self, model):
+        """содержимое экземпляров не равно"""
+        inst = model.objects.get(id=1)
+        inst1 = model()
+        assert inst != inst1
+
+    def test_different_models_equal(self, model, model2):
+        inst1 = model(name='name', type_test='test_type')
+        inst2 = model2(name='name', type_test='test_type')
+        assert inst1 != inst2
 
 
 def test_repr(model):
